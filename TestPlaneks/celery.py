20272ada956a8ea91ba643b51b0ebb84b003f78a
FedirYatsenko <f.yatsenko@knu.ua>
2021-08-11 17:45:43 +0300

    Added RabbitMQ url

--- TestPlaneks/celery.py
+++ TestPlaneks/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TestPlaneks.settings')
 
-app = Celery('TestPlaneks')
+app = Celery('TestPlaneks', broker=os.environ['CLOUDAMQP_URL'])
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
