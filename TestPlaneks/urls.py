2f6d98246553a241bfeec6ea51f12682732a96f0
FedirYatsenko <f.yatsenko@knu.ua>
2021-08-11 17:08:45 +0300

    Added 'form-control' class to inputs in AuthenticationForm

--- TestPlaneks/urls.py
+++ TestPlaneks/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView
+
+from .forms import CustomAuthForm
 
 from . import views
 
@@ -9,4 +11,5 @@ urlpatterns = [
     path('datasets/', include('datasets.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('login/', LoginView.as_view(authentication_form=CustomAuthForm), name='login'),
 ]  # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
